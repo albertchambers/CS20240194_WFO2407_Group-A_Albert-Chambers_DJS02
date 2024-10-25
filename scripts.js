7bcc35bece99ca91887a8bf6668d51a2744576c4
Albert Chambers <albertchambers09@gmail.com>
2024-10-25 10:18:17 +0200

    fix to try catch error

--- scripts.js
+++ scripts.js
@@ -5,17 +5,22 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  if (!dividend || !divider) {
-    result.innerText = 'Division not performed. Both values are required in inputs. Try again.'
-  } else if (dividend > divider) {
-    result.innerText = 'Division not performed. Invalid number provided. Try again.'
-    console.error('Invalid Numbers Entered');
-  } else if (isNaN(dividend) || isNaN(divider)) {
-    result.innerText = 'Something critical went wrong. Please reload the page.';
-    throw new Error('Critical Error: NaN Provided');
-  } else {
-    result.innerText = Math.floor(dividend / divider);
+  try {
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+
+    if (!dividend || !divider) {
+      result.innerText = 'Division not performed. Both values are required in inputs. Try again.'
+    } else if (isNaN(dividend) || isNaN(divider)) {
+      result.innerText = 'Something critical went wrong. Please reload the page.';
+      throw new Error('Critical Error: NaN Provided');
+    } else if (dividend > divider) {
+      result.innerText = 'Division not performed. Invalid number provided. Try again.'
+      console.error('Invalid Numbers Entered');
+    } else {
+      result.innerText = Math.floor(dividend / divider);
+    }
+  } catch (error) {
+    console.error(error.stack)
   }
 });
